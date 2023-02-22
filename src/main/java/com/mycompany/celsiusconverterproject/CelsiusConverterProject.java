c862965a3004cc7e37c547c63b74496efbc5907b
Oliver Wolff <oliver.wolff@mrjaffesclass.com>
2023-02-22 12:40:24 -0800

    first commit

---
+++ src/main/java/com/mycompany/celsiusconverterproject/CelsiusConverterProject.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.celsiusconverterproject;
+
+/**
+ *
+ * @author student
+ */
+public class CelsiusConverterProject {
+
+    
+}
